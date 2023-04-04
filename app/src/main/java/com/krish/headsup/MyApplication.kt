a67fc4532f776578d04aaaed46e5b2073e8ae022
Krishna Kant <kkant041@gmail.com>
2023-04-04 22:37:57 +0530

    add dependency injection

--- app/src/main/java/com/krish/headsup/MyApplication.kt
+++ app/src/main/java/com/krish/headsup/MyApplication.kt
@@ -1,11 +1,26 @@
 package com.krish.headsup
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.krish.headsup.services.api.ApiService
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
-class MyApplication : Application() {
+@HiltAndroidApp
+class MyApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         ApiService.init(this)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(hiltWorkerFactory)
+            .build()
     }
 }
