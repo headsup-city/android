49cc2131a12526b15ce5d9a1495a7f13144f3a82
Krishna Kant <kkant041@gmail.com>
2023-04-13 00:27:02 +0530

    basic bottom tab done

--- app/src/main/java/com/krish/headsup/MainActivity.kt
+++ app/src/main/java/com/krish/headsup/MainActivity.kt
@@ -2,11 +2,11 @@ package com.krish.headsup
 
 import android.os.Bundle
 import android.view.View
-import android.widget.FrameLayout
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.krish.headsup.managers.AuthManager
 import com.krish.headsup.model.AuthState
@@ -20,10 +20,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var authState: AuthState
-
     private val authManager: AuthManager by lazy { (application as MyApplication).authManager }
+    private lateinit var onBackPressedCallback: OnBackPressedCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,39 +29,54 @@ class MainActivity : AppCompatActivity() {
 
         updateAuthStateFromToken()
 
-        val authNavHostFragment = supportFragmentManager.findFragmentById(R.id.authNavigation) as NavHostFragment
+        val authNavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.authNavigation) as NavHostFragment
         val authNavController = authNavHostFragment.navController
-        val loadingFragmentContainer = findViewById<FrameLayout>(R.id.loadingFragment)
+        val mainContainer = findViewById<FragmentContainerView>(R.id.mainContainer)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
-
-        // Initialize the navController
-        val mainNavHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavigation) as NavHostFragment
-        navController = mainNavHostFragment.navController
 
         authManager.authState.observe(this) { authState ->
             when (authState) {
                 AuthState.NO_USER -> {
-                    authNavController.navigate(R.id.action_global_greetingFragment) // Navigate to loginFragment
-                    loadingFragmentContainer.visibility = View.GONE
+                    authNavController.navigate(R.id.action_global_greetingFragment)
+                    authNavHostFragment.view?.visibility = View.VISIBLE
+                    mainContainer.visibility = View.GONE
                     bottomNavigationView.visibility = View.GONE
                 }
                 AuthState.LOADING -> {
-                    loadingFragmentContainer.visibility = View.VISIBLE
+                    authNavHostFragment.view?.visibility = View.GONE
+                    mainContainer.visibility = View.GONE
                     bottomNavigationView.visibility = View.GONE
                 }
                 AuthState.AUTHENTICATED -> {
-                    loadingFragmentContainer.visibility = View.GONE
+                    authNavHostFragment.view?.visibility = View.GONE
+                    mainContainer.visibility = View.VISIBLE
                     bottomNavigationView.visibility = View.VISIBLE
-                    setupBottomNavigationView(bottomNavigationView)
+
+                    setupBottomNavigationView(bottomNavigationView, mainContainer)
                 }
             }
         }
+
+        onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val currentNavController = mainContainer.getTag(R.id.mainContainer) as NavController
+                if (!currentNavController.navigateUp()) {
+                    if (this.isEnabled) {
+                        isEnabled = false
+                        finish()
+                    } else {
+                        isEnabled = true
+                    }
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
     private fun updateAuthStateFromToken() {
         val tokenManager = TokenManager(this)
         val accessToken = tokenManager.getTokenStore()?.access
-        val refreshToken = tokenManager.getTokenStore()?.refresh
 
         val newState = if (accessToken != null) {
             AuthState.AUTHENTICATED
@@ -74,72 +87,83 @@ class MainActivity : AppCompatActivity() {
         authManager.updateAuthState(newState)
     }
 
-    private fun setupBottomNavigationView(bottomNavigationView: BottomNavigationView) {
-        // Set up the navigation with the bottom navigation view
+    private fun setupBottomNavigationView(bottomNavigationView: BottomNavigationView, mainContainer: FragmentContainerView) {
         bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.homeTab -> {
-                    navController.navigate(R.id.homeTab)
-                    true
-                }
-                R.id.searchTab -> {
-                    navController.navigate(R.id.searchTab)
-                    true
-                }
-                R.id.createPostTab -> {
-                    // Handle your createPostTab logic
-                    true
-                }
-                R.id.activityTab -> {
-                    // Handle your activityTab logic
-                    true
-                }
-                R.id.profileTab -> {
-                    navController.navigate(R.id.profileTab)
-                    true
-                }
-                else -> false
+            val navGraphId = when (item.itemId) {
+                R.id.homeTab -> R.navigation.home_navigation
+                R.id.searchTab -> R.navigation.search_navigation
+                R.id.createPostTab -> R.navigation.create_post_navigation
+                R.id.activityTab -> R.navigation.activity_navigation
+                R.id.profileTab -> R.navigation.profile_navigation
+                else -> return@setOnItemSelectedListener false
             }
+
+            val inflater = NavHostFragment.create(navGraphId)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainContainer, inflater)
+                .commitNow()
+
+            val navController = inflater.navController
+            mainContainer.setTag(R.id.mainContainer, navController)
+
+            setupDestinationChangeListener(navController, bottomNavigationView)
+
+            true
         }
 
-        bottomNavigationView.setupWithNavController(navController)
-
+        // Set default tab
+        bottomNavigationView.selectedItemId = R.id.homeTab
         setupIcons(bottomNavigationView)
+    }
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.conversationFragment,
-                R.id.profileFragment,
-                R.id.textPostFragment,
-                R.id.imagePostFragment,
-                R.id.videoPostFragment -> {
-                    bottomNavigationView.visibility = View.GONE
-                }
-                else -> {
-                    bottomNavigationView.visibility = View.VISIBLE
-                }
+    private fun handleBottomNavigationViewVisibility(
+        destinationId: Int,
+        bottomNavigationView: BottomNavigationView
+    ) {
+        when (destinationId) {
+            R.id.conversationFragment,
+            R.id.profileFragment,
+            R.id.textPostFragment,
+            R.id.imagePostFragment,
+            R.id.videoPostFragment -> {
+                bottomNavigationView.visibility = View.GONE
+            }
+            else -> {
+                bottomNavigationView.visibility = View.VISIBLE
             }
         }
     }
 
-
+    private fun setupDestinationChangeListener(
+        navController: NavController,
+        bottomNavigationView: BottomNavigationView
+    ) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            handleBottomNavigationViewVisibility(destination.id, bottomNavigationView)
+        }
+    }
 
     private fun setupIcons(bottomNavigationView: BottomNavigationView) {
         // Set icons for BottomNavigationView menu items
-        bottomNavigationView.menu.findItem(R.id.homeTab).icon = IconicsDrawable(this, FontAwesome.Icon.faw_home).apply {
-            sizeDp = 24
-        }
-        bottomNavigationView.menu.findItem(R.id.searchTab).icon = IconicsDrawable(this, Ionicons.Icon.ion_ios_search).apply {
-            sizeDp = 24
-        }
-        bottomNavigationView.menu.findItem(R.id.createPostTab).icon = IconicsDrawable(this, Ionicons.Icon.ion_android_add).apply {
-            sizeDp = 24
-        }
-        bottomNavigationView.menu.findItem(R.id.activityTab).icon = IconicsDrawable(this, FontAwesome.Icon.faw_bell).apply {
-            sizeDp = 24
-        }
-        bottomNavigationView.menu.findItem(R.id.profileTab).icon = IconicsDrawable(this, FontAwesome.Icon.faw_user).apply {
-            sizeDp = 24
-        }
+        bottomNavigationView.menu.findItem(R.id.homeTab).icon =
+            IconicsDrawable(this, FontAwesome.Icon.faw_home).apply {
+                sizeDp = 24
+            }
+        bottomNavigationView.menu.findItem(R.id.searchTab).icon =
+            IconicsDrawable(this, Ionicons.Icon.ion_ios_search).apply {
+                sizeDp = 24
+            }
+        bottomNavigationView.menu.findItem(R.id.createPostTab).icon =
+            IconicsDrawable(this, Ionicons.Icon.ion_android_add).apply {
+                sizeDp = 24
+            }
+        bottomNavigationView.menu.findItem(R.id.activityTab).icon =
+            IconicsDrawable(this, FontAwesome.Icon.faw_bell).apply {
+                sizeDp = 24
+            }
+        bottomNavigationView.menu.findItem(R.id.profileTab).icon =
+            IconicsDrawable(this, FontAwesome.Icon.faw_user).apply {
+                sizeDp = 24
+            }
     }
 }
